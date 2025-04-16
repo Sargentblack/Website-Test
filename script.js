bbff3062f84adb5fbb5bfa8a0e2494b3addac7ea
Sargentblack <167802501+Sargentblack@users.noreply.github.com>
2025-04-16 04:36:17 +0200

    Create Website files

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('clickMe').addEventListener('click', function() {
+  document.getElementById('message').textContent = 'You clicked the button! This is an Example of JavaScript interaction!';
+});
