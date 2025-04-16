f5f8ec4ef14e943d93b3f758a280bf16eb0ecd56
Sargentblack <167802501+Sargentblack@users.noreply.github.com>
2025-04-16 04:39:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('clickMe').addEventListener('click', function() {
-  document.getElementById('message').textContent = 'You clicked the button! This is an Example of JavaScript interaction!';
+  document.getElementById('message').textContent = 'This is an Example of a JavaScript interaction!';
 });
